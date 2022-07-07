1514a613dfa2ea13bb5512378099c358426b9768
maramendoza692 <mariagmenr@gmail.com>
2022-07-07 16:24:09 -0500

    Primer avance

---
+++ dashboard/dashboard.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Calificacion } from '../../_model/calificacion';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTooltip,
+  ApexYAxis,
+  ApexPlotOptions,
+  ApexStroke,
+  ApexLegend,
+  ApexNonAxisChartSeries,
+  ApexMarkers,
+  ApexGrid,
+  ApexTitleSubtitle,
+} from "ng-apexcharts";
+import { FormGroup, Validators } from "@angular/forms";
+import { Materia } from '../../_model/materia';
+export type areaChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  stroke: ApexStroke;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+  legend: ApexLegend;
+  colors: string[];
+};
+
+export type restRateChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  dataLabels: ApexDataLabels;
+  markers: ApexMarkers;
+  colors: string[];
+  yaxis: ApexYAxis;
+  grid: ApexGrid;
+  tooltip: ApexTooltip;
+  legend: ApexLegend;
+  title: ApexTitleSubtitle;
+};
+export type performanceRateChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  dataLabels: ApexDataLabels;
+  markers: ApexMarkers;
+  colors: string[];
+  yaxis: ApexYAxis;
+  grid: ApexGrid;
+  tooltip: ApexTooltip;
+  legend: ApexLegend;
+  title: ApexTitleSubtitle;
+};
+
+export type radialChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels: string[];
+  colors: string[];
+  plotOptions: ApexPlotOptions;
+};
+@Component({
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.sass"],
+})
+export class DashboardComponent implements OnInit {
+  @ViewChild("chart") chart: ChartComponent;
+  public areaChartOptions: Partial<areaChartOptions>;
+  public radialChartOptions: Partial<radialChartOptions>;
+  public restRateChartOptions: Partial<restRateChartOptions>;
+  public performanceRateChartOptions: Partial<performanceRateChartOptions>;
+
+  displayedColumns = [
+    "clave",
+    "nombre",
+    "profesor",
+    "calificacion"
+    
+  ];
+
+  dataArray: Calificacion[];
+  form: FormGroup;
+  formBuilder: any;
+
+  constructor() {
+   
+  }
+  ngOnInit() {
+  
+    this.form = this.formBuilder.group({  
+      calificacion:['', Validators.required]});
+  }
+  
+    
+  
+}
